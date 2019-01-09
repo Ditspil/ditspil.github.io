dd3fe666c1aa68b823abb1e0802f154898e98352
Johannes Frandsen <jsf@greenoak.dk>
2019-01-09 17:29:07 +0100

    Small cleanup

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+<b-container>
+  <b-row>
+    <b-col></b-col>
+    <b-col>
+      <b-card title="Om ditspil.dk">
+        <p class="card-text">
+          Siden er endu ikke live. Vi arbejder på det.
+        </p>
+        <v-icon name="dice" scale="5" />
+      </b-card>
+    </b-col>
+    <b-col></b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/dice'
+
+export default {
+  name: 'about',
+  components: {
+    'v-icon': Icon
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<b-jumbotron header="Ditspil.dk" lead="Siden er ikke live endnu.">
+  <v-icon name="dice" scale="10" />
+</b-jumbotron>
+</template>
+
+<script>
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/dice'
+
+export default {
+  name: 'home',
+  components: {
+    'v-icon': Icon
+  }
+}
+</script>
+
+<style scoped>
+</style>
